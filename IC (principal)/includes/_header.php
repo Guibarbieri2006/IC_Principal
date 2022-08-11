1a2123e8647b8425d975f7dc081495a72b51f8da
Guilherme Barbieri <93358510+Guibarbieri2006@users.noreply.github.com>
2022-08-11 12:27:40 -0300

    Add files via upload

---
+++ IC (principal)/includes/_header.php
@@ -0,0 +1,23 @@
+<header>
+    <nav class="navbar navbar-expand-lg navbar-dark">
+        
+        <img src="./content/logo-ic1.png">
+        
+        <a class="navbar-brand" href="#">IC</a>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="./index.php">Promoção</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./produtos.php">Produtos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./contato.php">Contato</a>
+                </li>
+
+            </ul>
+        </div>
+    </nav>
+</header>
+<main>
